931aad46fcff17cab268efd56d8dcc3728588b9b
Benjamin Tambourine <vtambourine@ya.ru>
2013-02-16 04:33:46 +0400

    Удалить старые установочные скрипты.
    Установка и раскладка скриптов теперь вся через bootstrap.sh

--- deploy.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-for server in "$@"; do
-	ssh $server	< fetch.sh
-done
--- fetch.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-cd
-if [ ! -d dotfiles ]; then
-	git clone https://github.com/vtambourine/dotfiles.git
-fi
-cd dotfiles
-set -- -f; source bootstrap.sh
