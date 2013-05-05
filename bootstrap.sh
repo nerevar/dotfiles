51ccd2823de4f747d6e899fd12a868c07e8a5e84
Roman Rybalchenko (nerevar) <nerevar@yandex-team.ru>
2013-05-05 17:02:44 +0000

    Update remove or self

--- bootstrap.sh
+++ bootstrap.sh
@@ -32,6 +32,7 @@ done
 
 # Deploy dotfiles on the remote host if one is provided
 if [ $# -gt 0 ]; then
+
     while [ $1 ]; do
         ssh $1 -- "curl -kfsSL $DOTFILES_BOOTSTRAP \$1 | bash -s -- --force"
         if [[ with_keys ]]; then
@@ -41,64 +42,67 @@ if [ $# -gt 0 ]; then
         shift
     done
     exit 0
-fi
 
-# Download and extract the repository to the home directory if the directory is missing
-if [ ! -d ${DOTFILES_DIRECTORY} ]; then
-    printf "Downloading dotfiles...\n"
-    mkdir -p ${DOTFILES_DIRECTORY}
-    # Get the tarball
-    curl -kfsSLo ${HOME}/dotfiles.tar.gz ${DOTFILES_TARBALL_PATH}
-    # Extract to the dotfiles directory
-    tar -zxf ${HOME}/dotfiles.tar.gz --strip-components 1 -C ${DOTFILES_DIRECTORY}
-    # Remove the tarball
-    rm -rf ${HOME}/dotfiles.tar.gz
-fi
+else
 
-cd ${DOTFILES_DIRECTORY}
-
-source ./lib/utils.sh
-
-# Initialize the git repository if it's missing
-if type_exists 'git'; then
-    if is_git_repo; then
-        echo "Updating git repository..."
-        git pull --rebase origin ${DOTFILES_GIT_BRANCH}
-    else
-        echo "Initializing git repository..."
-        git init
-        git remote add origin ${DOTFILES_GIT_REMOTE}
-        git fetch origin ${DOTFILES_GIT_BRANCH}
-        # Reset the index and working tree to the fetched HEAD
-        # (submodules are cloned in the subsequent sync step)
-        git reset --hard FETCH_HEAD
-        # Remove any untracked files
-        git clean -fd
-    fi
-fi
-
-mirrorfiles() {
-    # Force remove the vim directory if it's already there.
-    if [ -e "${HOME}/.vim" ]; then
-        rm -rf "${HOME}/.vim"
+    # Download and extract the repository to the home directory if the directory is missing
+    if [ ! -d ${DOTFILES_DIRECTORY} ]; then
+        printf "Downloading dotfiles...\n"
+        mkdir -p ${DOTFILES_DIRECTORY}
+        # Get the tarball
+        curl -kfsSLo ${HOME}/dotfiles.tar.gz ${DOTFILES_TARBALL_PATH}
+        # Extract to the dotfiles directory
+        tar -zxf ${HOME}/dotfiles.tar.gz --strip-components 1 -C ${DOTFILES_DIRECTORY}
+        # Remove the tarball
+        rm -rf ${HOME}/dotfiles.tar.gz
     fi
 
-    # Create ssh directory if necessary.
-    if [ ! -e "${HOME}/.ssh" ]; then
-        mkdir -m 0700 "${HOME}/.ssh"
+    cd ${DOTFILES_DIRECTORY}
+
+    source ./lib/utils.sh
+
+    # Initialize the git repository if it's missing
+    if type_exists 'git'; then
+        if is_git_repo; then
+            echo "Updating git repository..."
+            git pull --rebase origin ${DOTFILES_GIT_BRANCH}
+        else
+            echo "Initializing git repository..."
+            git init
+            git remote add origin ${DOTFILES_GIT_REMOTE}
+            git fetch origin ${DOTFILES_GIT_BRANCH}
+            # Reset the index and working tree to the fetched HEAD
+            # (submodules are cloned in the subsequent sync step)
+            git reset --hard FETCH_HEAD
+            # Remove any untracked files
+            git clean -fd
+        fi
     fi
 
-    # Create the necessary symbolic links between the `dotfiles` and `HOME`
-    # directory. The `bash_profile` sources other files directly from the
-    # `.dotfiles` repository.
-    link "bash/bash_profile"  ".bash_profile"
-    link "git/gitconfig"      ".gitconfig"
-    link "git/gitignore"      ".gitignore"
-    link "vim"                ".vim"
-    link "vim/vimrc"          ".vimrc"
+    mirrorfiles() {
+        # Force remove the vim directory if it's already there.
+        if [ -e "${HOME}/.vim" ]; then
+            rm -rf "${HOME}/.vim"
+        fi
 
-    printf "Dotfiles update complete!\n"
-}
+        # Create ssh directory if necessary.
+        if [ ! -e "${HOME}/.ssh" ]; then
+            mkdir -m 0700 "${HOME}/.ssh"
+        fi
 
-mirrorfiles
-source ${HOME}/.bash_profile
+        # Create the necessary symbolic links between the `dotfiles` and `HOME`
+        # directory. The `bash_profile` sources other files directly from the
+        # `.dotfiles` repository.
+        link "bash/bash_profile"  ".bash_profile"
+        link "git/gitconfig"      ".gitconfig"
+        link "git/gitignore"      ".gitignore"
+        link "vim"                ".vim"
+        link "vim/vimrc"          ".vimrc"
+
+        printf "Dotfiles update complete!\n"
+    }
+
+    mirrorfiles
+    source ${HOME}/.bash_profile
+
+fi
