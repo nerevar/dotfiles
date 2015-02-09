20142b2dfc5ffe69fb3e3d6a496438597f45cfe1
Roman Rybalchenko (nerevar) <nerevar@yandex-team.ru>
2015-02-09 16:13:48 +0300

    full remove vim

--- vim/install.sh
+++
@@ -1,8 +0,0 @@
-echo Fetch/update neobundle.vim
-rm -rf ~/.vim/bundle/neobundle.vim
-wget -q https://codeload.github.com/Shougo/neobundle.vim/tar.gz/master
-mkdir -p ~/.vim/bundle/
-tar xf master
-mv neobundle.vim-master ~/.vim/bundle/neobundle.vim
-rm master
-echo Done
